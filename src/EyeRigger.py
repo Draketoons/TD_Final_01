c3bcee7340a5ffadee85029554ebaa2cfc179e45
Draketoons <mattadragon5@gmail.com>
2025-05-05 18:34:10 -0500

    Bug Fixes
    Eye rigger now properly creates the ctrls and grps needed for a basic eye control rig

--- src/EyeRigger.py
+++ src/EyeRigger.py
@@ -2,14 +2,13 @@ from PySide2.QtGui import QColor
 import maya.cmds as mc
 import maya.mel as mel
 
-from PySide2.QtWidgets import (QWidget,
-                               QListWidget,
+from PySide2.QtWidgets import (QListWidget,
                                QVBoxLayout,
                                QHBoxLayout,
-                               QLabel,
                                QPushButton,
-                               QLineEdit,
-                               QMessageBox)
+                               QMessageBox,
+                               QSlider,
+                               QLabel)
 
 from PySide2.QtCore import Qt
 from MayaUtils import QMayaWindow
@@ -25,7 +24,7 @@ def TryAction(action):
 
 class EyeRigger:
     def __init__(self):
-        self.controllerSize = 5
+        self.controllerSize = 1
     
     def AddMeshes(self):
         selection = mc.ls(sl=True)
@@ -52,18 +51,28 @@ class EyeRigger:
         
         self.leftEye = self.meshes[0]
         self.rightEye = self.meshes[1]
+        self.eyeGrp = mc.group(self.leftEye, self.rightEye, name="Eye_Grp")
 
         for eye in [self.leftEye, self.rightEye]:
-            ctrl =  mc.circle(name=eye + "_Ctrl", radius=0.3)[0]
+            ctrl =  mc.circle(name=eye + "_Ctrl", radius=0.3 * self.controllerSize)[0]
             grp = mc.group(ctrl, name=eye + "_Ctrl_Grp")
             mc.matchTransform(grp, eye)
             mc.aimConstraint(ctrl, eye, aimVector = (0,0,1), upVector=(0,1,0), worldUpType = "scene")
+        
+        leftEyeLoc = mc.xform(self.leftEye, query=True, translation=True, worldSpace=False)
+        rightEyeLoc = mc.xform(self.rightEye, query=True, translation=True, worldSpace=False)
+        eyeGrpRot = mc.xform(self.eyeGrp, query=True, rotation=True, worldSpace=False)
+        cX = (leftEyeLoc[0] + rightEyeLoc[0]) / 2
+        cY = (leftEyeLoc[1] + rightEyeLoc[1]) / 2
+        cZ = (leftEyeLoc[2] + rightEyeLoc[2]) / 2
 
-        mainCtrl = mc.circle(name = "Main_EyeCtrl", radius=0.5)[0]
-        mainGrp = mc.group(ctrl, name="Main_Ctrl_Grp")
-        mc.matchTransform(mainGrp, self.leftEye)
+        mainCtrl = mc.circle(name = "Main_EyeCtrl", radius=1 * self.controllerSize)[0]
+        mainGrp = mc.group(mainCtrl, name="Main_Ctrl_Grp")
 
-        mc.parent(self.leftEye + "_CTRL_GRP", self.rightEye + "_CTRL_GRP", mainCtrl)
+        mc.xform(mainGrp, translation = (cX,cY,cZ), rotation=(eyeGrpRot[0], eyeGrpRot[1], eyeGrpRot[2]))
+        mc.parent(self.leftEye + "_Ctrl_Grp", self.rightEye + "_Ctrl_Grp", mainCtrl)
+        mc.xform(mainGrp, translation = (cX,cY,cZ + 5))
+
         print("Rig Set!")
 
 
@@ -78,9 +87,25 @@ class EyeRiggerToolWidget(QMayaWindow):
 
         self.meshList = QListWidget()
         self.masterLayout.addWidget(self.meshList)
+
         addMeshBtn = QPushButton("Add Meshses")
         addMeshBtn.clicked.connect(self.AddMeshBtnClicked)
         self.masterLayout.addWidget(addMeshBtn)
+        
+        clearMeshBtn = QPushButton("Clear Meshes")
+        clearMeshBtn.clicked.connect(self.ClearMeshesBtnClicked)
+        self.masterLayout.addWidget(clearMeshBtn)
+
+        ctrlSizeSliderLayout = QHBoxLayout()
+        ctrlSizeSlider = QSlider()
+        ctrlSizeSlider.setValue(self.rigger.controllerSize)
+        ctrlSizeSlider.valueChanged.connect(self.CtrlSizeValueChanged)
+        ctrlSizeSlider.setRange(0.01, 10)
+        ctrlSizeSlider.setOrientation(Qt.Horizontal)
+        ctrlSizeSliderLayout.addWidget(ctrlSizeSlider)
+        self.ctrlSizeLabel = QLabel(f"{self.rigger.controllerSize}")
+        self.masterLayout.addWidget(self.ctrlSizeLabel)
+        self.masterLayout.addLayout(ctrlSizeSliderLayout)
 
         rigEyesBtn = QPushButton("Rig Eyes")
         rigEyesBtn.clicked.connect(self.RiggEyesBtnClicked)
@@ -97,6 +122,14 @@ class EyeRiggerToolWidget(QMayaWindow):
     @TryAction
     def RiggEyesBtnClicked(self):
         self.rigger.RigEyes()
+
+    @TryAction
+    def ClearMeshesBtnClicked(self):
+        self.meshList.clear()
+
+    def CtrlSizeValueChanged(self, newValue):
+        self.rigger.controllerSize = newValue
+        self.ctrlSizeLabel.setText(f"{self.rigger.controllerSize}")
 
 def Run():
     eyeRiggerToolWidget = EyeRiggerToolWidget()
